c03410be69f21a882fa41a3721708b7be4c8e989
DavidHruby1 <hrubyd74@gmail.com>
2025-08-22 16:01:48 +0200

    Update manifest and background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,14 @@
+
+// enables the side panel to open when the extension icon is clicked
 chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+
+// listens for the keyboard shortcut to toggle the side panel using "Alt+Shift+V"
+chrome.commands.onCommand.addListener((command) => {
+	if (command === "toggle-side-panel") {
+ 	    chrome.windows.getCurrent(async (window) => {
+        	if (window?.id) {
+            	await chrome.sidePanel.open({ windowId: window.id });
+        	}
+    	});
+	}
+});
