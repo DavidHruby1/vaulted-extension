6cb25229ee746fb2ee5cefb2ce1cd6e432c9a015
DavidHruby1 <hrubyd74@gmail.com>
2025-08-27 14:47:50 +0200

    Removed unnecessary files

---
+++ src/sidepanel/components/prompts/PromptsContainer.tsx
@@ -0,0 +1,9 @@
+import styles from './PromptSContainer.module.css';
+
+export const PromptsContainer = () => {
+    return (
+        <div className={ styles.container }>
+            {/* The PromptCards go here */}
+        </div>
+    );
+}
