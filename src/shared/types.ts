a6922228e1cf48f47a0b40c7ac6be0bc353634ac
DavidHruby1 <hrubyd74@gmail.com>
2025-08-28 10:53:49 +0200

    create Prompt interface in type.ts

---
+++ src/shared/types.ts
@@ -0,0 +1,9 @@
+
+export interface Prompt {
+    id: string,
+    title: string,
+    tags: string[],
+    text: string,
+    createdAt: number, // I will use Date.now() to convert date to a num for easier sorting and storing
+    tokenCount?: number // For now its optional
+}
