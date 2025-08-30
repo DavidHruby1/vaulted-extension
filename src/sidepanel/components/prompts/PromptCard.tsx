5c071ab444c408262de85a2f48faf37879eb0c38
DavidHruby1 <hrubyd74@gmail.com>
2025-08-30 10:48:25 +0200

    feat: create PromptCard component

---
+++ src/sidepanel/components/prompts/PromptCard.tsx
@@ -0,0 +1,34 @@
+import styles from './PromptCard.module.css';
+import { Prompt } from '@/shared/types';
+import { Copy, Syringe, EllipsisVertical } from 'lucide-react';
+
+interface PromptCardProps {
+    prompt: Prompt;
+}
+
+export const PromptCard = ({ prompt }: PromptCardProps) => {
+    return (
+        <div className={ styles.card }>
+            <div className={ styles['card-header'] }>
+                <h3>{ prompt.title }</h3>
+                <span>{ prompt.tokenCount }</span>
+                <div className={ styles.actions }>
+                    <button>
+                        <Copy size={32} strokeWidth={3} />
+                    </button>
+                    <button>
+                        <Syringe size={32} strokeWidth={3} />
+                    </button>
+                    <button>
+                        <EllipsisVertical size={32} strokeWidth={3} />
+                    </button>
+                </div>
+            </div>
+            <div className={ styles['card-body'] }>
+                <p className={ styles['card-text'] }>
+                    { prompt.text }
+                </p>
+            </div>
+        </div>
+    );
+}
