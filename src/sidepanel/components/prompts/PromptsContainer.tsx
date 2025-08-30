266568f008eee4e9b6eaefffff90baf5f8f48d30
DavidHruby1 <hrubyd74@gmail.com>
2025-08-30 11:13:16 +0200

    feat: create PromptsContainer

--- src/sidepanel/components/prompts/PromptsContainer.tsx
+++ src/sidepanel/components/prompts/PromptsContainer.tsx
@@ -1,4 +1,10 @@
 import styles from './PromptSContainer.module.css';
+import { Prompt } from '@/shared/types';
+import { PromptCard } from '@components/prompts/PromptCard';
+
+interface PromptContainerProps {
+    prompts: Prompt[];
+}
 
 export const PromptsContainer = () => {
     return (
